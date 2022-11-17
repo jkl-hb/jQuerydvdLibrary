c8eaab49d91b8073218e683188b7e4c2f8ebae34
Jennifer Ly <k.ly.jennifer@outlook.com>
2022-11-17 10:16:46 -0500

    fixed add

--- js/home.js
+++ js/home.js
@@ -164,47 +164,47 @@ function checkAndDisplayValidationErrors(input) {
 }
 
 function clearDVDMenu() {
-    $('#createButton').on("click", function(){
+    $('#createButton').on("click", function () {
 
         $('#dvdTable').hide();
         $('#createButton').hide();
 
-        })
+    })
 
-    };
+};
 
-    function addDvd() {
-        $('#createButton').click(function (event) {
-            $.ajax({
-               type: 'POST',
-               url: 'http://dvd-library.us-east-1.elasticbeanstalk.com/dvds',
-               data: JSON.stringify({
-                    title: $('#addDVDTitle').val(),
-                    releaseYear: $('#addReleaseYear').val(),
-                    director: $('#addDirector').val(),
-                    rating: $('#addRating').val(),
-                    notes: $('#addNotes').val()
-               }),
-               headers: {
-                   'Accept': 'application/json',
-                   'Content-Type': 'application/json'
-               },
-               'dataType': 'json',
-               success: function() {
-                   $('#errorMessages').empty();
-                   $('#addDVDTitle').val('');
-                   $('#addReleaseYear').val('');
-                   $('#addDirector').val('');
-                   $('#addRating').val('');
-                   $('#addNotes').val('');
-                   loadDvds();
-               },
-               error: function () {
-                   $('#errorMessages')
+function addDvd() {
+    $('#createButton').click(function (event) {
+        $.ajax({
+            type: 'POST',
+            url: 'http://dvd-library.us-east-1.elasticbeanstalk.com/dvd',
+            data: JSON.stringify({
+                title: $('#addDVDTitle').val(),
+                releaseYear: $('#addReleaseYear').val(),
+                director: $('#addDirector').val(),
+                rating: $('#addRating').val(),
+                notes: $('#addNotes').val()
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            'dataType': 'json',
+            success: function () {
+                $('#errorMessages').empty();
+                $('#addDVDTitle').val('');
+                $('#addReleaseYear').val('');
+                $('#addDirector').val('');
+                $('#addRating').val('');
+                $('#addNotes').val('');
+                loadDvds();
+            },
+            error: function () {
+                $('#errorMessages')
                     .append($('<li>')
-                    .attr({class: 'list-group-item list-group-item-danger'})
-                    .text('Error calling web service. Please try again later.')); 
-               }
-            })
-        });
-    }
+                        .attr({ class: 'list-group-item list-group-item-danger' })
+                        .text('Error calling web service. Please try again later.'));
+            }
+        })
+    });
+}
